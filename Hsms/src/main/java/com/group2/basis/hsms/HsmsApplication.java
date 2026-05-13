eab78a97714cc7094fe32ddfb9835a34cff8f15d
DuyKhanh1802 <phamduykhanhhe190351@gmail.com>
2026-05-13 22:06:08 +0700

    deploy new project

---
+++ Hsms/src/main/java/com/group2/basis/hsms/HsmsApplication.java
@@ -0,0 +1,13 @@
+package com.group2.basis.hsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsmsApplication.class, args);
+    }
+
+}
---
+++ Hsms/src/test/java/com/group2/basis/hsms/HsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.basis.hsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
