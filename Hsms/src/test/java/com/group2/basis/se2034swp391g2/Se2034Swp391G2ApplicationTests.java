612652a55f642c847dcf615dce0c8a2ad9df9673
DuyKhanh1802 <phamduykhanhhe190351@gmail.com>
2026-05-21 11:34:03 +0700

    Initial structure project

---
+++ Hsms/src/test/java/com/group2/basis/se2034swp391g2/Se2034Swp391G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.basis.se2034swp391g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2034Swp391G2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
