7b4f52ac6665d11f3be552772961bf8c76ab9033
Van Huy <dovanhuyb@gmail.com>
2026-05-26 01:08:42 +0700

    AdminDashboardScreen

---
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/AdminDashboardController.java
@@ -0,0 +1,13 @@
+package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminDashboardController {
+
+    @GetMapping("/admin/dashboard")
+    public String dashboard() {
+        return "Admin/Dashboard";
+    }
+}
