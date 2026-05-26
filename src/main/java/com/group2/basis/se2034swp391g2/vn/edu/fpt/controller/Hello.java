b00c224ae6112407d06122c1288bee9e1d508e9c
Van Huy <dovanhuyb@gmail.com>
2026-05-26 16:01:30 +0700

    AdminDashboardScreen

---
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/Hello.java
@@ -0,0 +1,8 @@
+package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        
+    }
+}
