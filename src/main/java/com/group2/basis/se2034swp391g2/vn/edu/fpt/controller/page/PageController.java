83e910ec5d37d819a4f4d4f48366e1bdf447fa5f
DuyKhanh1802 <phamduykhanhhe190351@gmail.com>
2026-05-26 15:56:33 +0700

    khánh push fragment,layout

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/page/PageController.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/page/PageController.java
@@ -1,7 +1,14 @@
 package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @GetMapping({"/","/home"})
+    public String index(Model model){
+        model.addAttribute("pageTitle","Audo Residence");
+        return "Guest/HomePage";
+    }
 }
