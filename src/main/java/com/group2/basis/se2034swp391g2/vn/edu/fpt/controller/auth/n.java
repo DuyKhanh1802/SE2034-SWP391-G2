985cae2991deb5116a954668a724ae72c677d2d4
DuyKhanh1802 <phamduykhanhhe190351@gmail.com>
2026-05-27 16:11:42 +0700

    fix ten package

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/auth/n.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/auth/n.java
@@ -1,4 +1,4 @@
-package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.Auth;
+package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.auth;
 
 public class n {
 }
--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/guest/j.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/guest/j.java
@@ -1,4 +1,4 @@
-package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.Guest;
+package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.guest;
 
 public class j {
 }
