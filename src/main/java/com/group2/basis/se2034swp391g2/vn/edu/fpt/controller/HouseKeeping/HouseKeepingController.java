7a8372f650fabf1dbc2fb27fa2c2c07a855693fc
yennhhe187368 <yennhhe187368@fpt.edu.vn>
2026-05-27 17:13:55 +0700

    move class housekeeping vào trong package

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/HouseKeeping/HouseKeepingController.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/HouseKeeping/HouseKeepingController.java
@@ -1,4 +1,4 @@
-package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller;
+package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.HouseKeeping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
