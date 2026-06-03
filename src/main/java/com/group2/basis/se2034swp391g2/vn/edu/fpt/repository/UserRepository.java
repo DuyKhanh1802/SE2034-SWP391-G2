1808282aae9f325c3b01ce34c5d34a576e88fcee
DuyKhanh1802 <phamduykhanhhe190351@gmail.com>
2026-06-03 19:35:20 +0700

    khánh commit login/resgister

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/repository/UserRepository.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.group2.basis.se2034swp391g2.vn.edu.fpt.repository;
 
 import com.group2.basis.se2034swp391g2.vn.edu.fpt.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByIdentityCard(String identityCard);
     Optional<User> findByPhone(String phone);
+
 }
