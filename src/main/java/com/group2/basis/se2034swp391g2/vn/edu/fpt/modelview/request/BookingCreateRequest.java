01918ccf61fa5bee5c7c30545609e4c74d066cf8
Van Huy <dovanhuyb@gmail.com>
2026-06-04 00:52:17 +0700

    Receptionist_listbooking

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/modelview/request/BookingCreateRequest.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/modelview/request/BookingCreateRequest.java
@@ -1,4 +1,34 @@
 package com.group2.basis.se2034swp391g2.vn.edu.fpt.modelview.request;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class BookingCreateRequest {
+
+    private String fullName;
+
+    private String phoneNumber;
+
+    private String email;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkInDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkOutDate;
+
+    private Integer adults;
+
+    private Integer children;
+
+    private List<Long> roomIds;
+
+    private String notes;
+
+    // create-only hoặc create-check-in
+    private String action;
 }
--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/modelview/response/BookingResponse.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/modelview/response/BookingResponse.java
@@ -1,4 +1,21 @@
 package com.group2.basis.se2034swp391g2.vn.edu.fpt.modelview.response;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponse {
+
+    private Long bookingId;
+    private String bookingReference;
+    private String guestName;
+    private String roomName;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String status;
+    private BigDecimal totalAmount;
+
+
 }
