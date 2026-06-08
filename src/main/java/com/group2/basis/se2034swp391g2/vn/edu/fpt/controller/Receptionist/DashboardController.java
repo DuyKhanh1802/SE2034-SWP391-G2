faf58ef415e4914fd388f3b2cfe185a00e7d6951
Van Huy <dovanhuyb@gmail.com>
2026-06-08 20:19:16 +0700

    huy comit 8/6 2

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/Receptionist/DashboardController.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/Receptionist/DashboardController.java
@@ -1,13 +1,16 @@
 package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.Receptionist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("/receptionist/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("pageTitle", "TỔNG QUAN LỄ TÂN");
+        model.addAttribute("activeMenu", "dashboard");
         return "receptionist/Dashboard";
     }
 }
