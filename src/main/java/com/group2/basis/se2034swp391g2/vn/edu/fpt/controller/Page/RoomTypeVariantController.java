ee05e394ad6f28db6b21ad0fad61cabd13dc0fbf
DuyKhanh1802 <he190351phamduykhanh@gmail.com>
2026-06-16 17:51:13 +0700

    khánh commit 16/6 homepage_roomtype_fix

--- src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/Page/RoomTypeVariantController.java
+++ src/main/java/com/group2/basis/se2034swp391g2/vn/edu/fpt/controller/Page/RoomTypeVariantController.java
@@ -1,61 +1,83 @@
-package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.Page;
+    package com.group2.basis.se2034swp391g2.vn.edu.fpt.controller.Page;
 
-import com.group2.basis.se2034swp391g2.vn.edu.fpt.model.RoomType;
-import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.RoomTypeRepository;
-import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.RoomTypeVariantRepository;
-import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.projection.GuestRoomVariantProjection;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import lombok.*;
+    import com.group2.basis.se2034swp391g2.vn.edu.fpt.model.RoomType;
+    import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.RoomTypeRepository;
+    import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.RoomTypeVariantRepository;
+    import com.group2.basis.se2034swp391g2.vn.edu.fpt.repository.projection.GuestRoomVariantProjection;
+    import com.group2.basis.se2034swp391g2.vn.edu.fpt.service.RoomTypeVariantService;
+    import org.springframework.format.annotation.DateTimeFormat;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
+    import lombok.*;
 
-import java.time.LocalDate;
-import java.util.List;
+    import java.time.LocalDate;
+    import java.util.List;
 
-@RequiredArgsConstructor
-@RequestMapping("/page/rooms")
-@Controller
-public class RoomTypeVariantController {
+    @RequiredArgsConstructor
+    @RequestMapping("/page/rooms")
+    @Controller
+    public class RoomTypeVariantController {
 
-    private final RoomTypeVariantRepository roomTypeVariantRepository;
-    private final RoomTypeRepository roomTypeRepository;
-    @GetMapping
-    public String listRoomTypeVariant(
-            @RequestParam(name = "roomTypeId", required = false) Long roomTypeId,
-            @RequestParam(name = "viewType", required = false) String viewtype,
-            @RequestParam(name = "sort", defaultValue = "recommended") String sort,
-            @RequestParam(name = "checkInDate",required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate checkInDate,
-            @RequestParam(name = "checkOutDate", required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate checkOutDate,
-            @RequestParam(name = "adults",required = false, defaultValue = "1") Integer adults,
-            @RequestParam(name = "children",required = false,defaultValue = "0") Integer children,
-            @RequestParam(name = "roomCount", required = false,defaultValue = "1") Integer roomCount,
-            @RequestParam(name = "roomGuests", required = false) String roomGuests,
-            Model model
-    ){
-      List<RoomType> roomTypes = roomTypeRepository.findByIsDeletedFalse();
+        private final RoomTypeVariantService roomTypeVariantService;
 
-      List<GuestRoomVariantProjection> roomTypeVariant = roomTypeVariantRepository.findGuestRoomVariants(roomTypeId,viewtype,sort,checkInDate,checkOutDate,adults,children,roomCount);
-      model.addAttribute("roomTypes",roomTypes);
-      model.addAttribute("selectedRoomTypeId",roomTypeId);
-      model.addAttribute("selectedViewType", viewtype);
-      model.addAttribute("selectedSort",sort);
+        @GetMapping
+        public String listRoomTypeVariant(
+                @RequestParam(name = "roomTypeId", required = false) Long roomTypeId,
 
-      model.addAttribute("checkInDate",checkInDate);
-      model.addAttribute("checkOutDate",checkOutDate);
-      model.addAttribute("adults",adults);
-      model.addAttribute("children",children);
-      model.addAttribute("roomCount",roomCount);
-        model.addAttribute("roomGuests", roomGuests);
+                @RequestParam(name = "viewType", required = false) String viewType,
 
-      model.addAttribute("roomOptions", roomTypeVariant);
+                @RequestParam(name = "sort", defaultValue = "recommended") String sort,
 
-      return "page/RoomtypeVariant";
+                @RequestParam(name = "checkInDate", required = false)
+                @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                LocalDate checkInDate,
+
+                @RequestParam(name = "checkOutDate", required = false)
+                @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                LocalDate checkOutDate,
+
+                @RequestParam(name = "adults", required = false, defaultValue = "1") Integer adults,
+
+                @RequestParam(name = "children", required = false, defaultValue = "0") Integer children,
+
+                @RequestParam(name = "roomCount", required = false, defaultValue = "1") Integer roomCount,
+
+                @RequestParam(name = "roomGuests", required = false) String roomGuests,
+
+                Model model
+        ) {
+            List<RoomType> roomTypes = roomTypeVariantService.getRoomTypes();
+
+            List<GuestRoomVariantProjection> roomTypeVariants =
+                    roomTypeVariantService.getGuestRoomVariants(
+                            roomTypeId,
+                            viewType,
+                            sort,
+                            checkInDate,
+                            checkOutDate,
+                            adults,
+                            children,
+                            roomCount
+                    );
+
+            model.addAttribute("roomTypes", roomTypes);
+            model.addAttribute("roomOptions", roomTypeVariants);
+
+            model.addAttribute("selectedRoomTypeId", roomTypeId);
+            model.addAttribute("selectedViewType", viewType);
+            model.addAttribute("selectedSort", sort);
+
+            model.addAttribute("checkInDate", checkInDate);
+            model.addAttribute("checkOutDate", checkOutDate);
+            model.addAttribute("adults", adults);
+            model.addAttribute("children", children);
+            model.addAttribute("roomCount", roomCount);
+            model.addAttribute("roomGuests", roomGuests);
+
+            return "page/RoomTypeVariant";
+        }
     }
-}
+
